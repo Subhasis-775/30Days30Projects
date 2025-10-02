8b37e81594b591d1638f8efe1309cf1997345a39
Subhasis <subhasisrout00@gmail.com>
2025-10-02 19:22:16 +0530

    Added Color picker

---
+++ ColorPicker/src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [color,setColor]=useState("#ffffff");
+  const colourSet=()=>{
+    let chars="0123456789ABCDEF";
+    let newColor='#';
+    for(let i=0;i<6;i++){
+      newColor+=chars.charAt(Math.floor(Math.random()*chars.length));
+    }
+    setColor(newColor);
+  }
+  return (
+      <div  className="App"
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: color,
+        transition: "background-color 0.5s ease",
+      }}>
+        <button onClick={colourSet}  style={{
+          padding: "12px 24px",
+          fontSize: "16px",
+          borderRadius: "8px",
+          border: "none",
+          cursor: "pointer",
+          backgroundColor: "#fff",
+          color:"black"
+        }}>Generate random color</button>
+      </div>
+  )
+}
+
+export default App;
